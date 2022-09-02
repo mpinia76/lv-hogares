116ddfc0e1bffea51f3cd0b9b98cbbd207c7e1af
mpinia76 <marcos.pinero1976@gmail.com>
2022-09-02 17:25:27 -0300

    varios

--- resources/views/layouts/partials/head.blade.php
+++ resources/views/layouts/partials/head.blade.php
@@ -37,7 +37,7 @@
   <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
 
 <style>
-    
+
     .load{
         position: fixed;
         z-index: 9999;
--- resources/views/residentes/index.blade.php
+++ resources/views/residentes/index.blade.php
@@ -38,7 +38,7 @@
 
                     <!-- /.box-header -->
                         <div class="box-body">
-                            <table id="example1" class="table table-bordered table-striped">
+                            <table id="example1" class="table table-bordered table-striped nowrap" style="width:100%">
                                 <thead>
                                 <tr>
                                     <th>Nro.</th>
@@ -137,6 +137,7 @@
     <script>
         $(document).ready(function() {
             $('#example1').DataTable({
+                "responsive": true,
                 "language": {
                     "url": "//cdn.datatables.net/plug-ins/1.12.1/i18n/es-AR.json"
                 }
